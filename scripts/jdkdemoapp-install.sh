2e10da993ff4890cafe51487d06f688ff6003eb2
Gordonby <gordon.byers@microsoft.com>
2022-01-17 11:05:22 +0000

    fixing lower case issue

--- scripts/jdkdemoapp-install.sh
+++ scripts/jdkdemoapp-install.sh
@@ -1,10 +1,14 @@
-KVNAMELOWER=${AKVNAME,,}
-KVTENANT=$(az account show --query tenantId -o tsv)
+#!/bin/bash
+
+KVNAMELOWER=$(echo $AKVNAME | tr '[:upper:]' '[:lower:]')
 DNSNAME=${APPNAME}.${DNSDOMAIN}
 
+echo "Querying Azure CLI for tenant ID"
+KVTENANT=$(az account show --query tenantId -o tsv)
+
 echo 'Get the identity created from the KeyVaultSecret Addon'
-export CSISECRET_CLIENTID=$(az aks show -g $RG --name $AKSNAME --query addonProfiles.azureKeyvaultSecretsProvider.identity.clientId -o tsv)
+CSISECRET_CLIENTID=$(az aks show -g $RG --name $AKSNAME --query addonProfiles.azureKeyvaultSecretsProvider.identity.clientId -o tsv)
 echo $CSISECRET_CLIENTID
 
 helm upgrade --install $APPNAME $APPURI --set nameOverride="${APPNAME}",frontendCertificateSource="${CERTSOURCE}",csisecrets.vaultname="${KVNAMELOWER}",csisecrets.tenantId="${KVTENANT}",csisecrets.clientId="${CSISECRET_CLIENTID}",dnsname="${DNSNAME}",appgw.frontendCertificateName="${APPNAME}-fe",appgw.rootCertificateName="${APPNAME}",letsEncrypt.issuer="${LEISSUER}",letsEncrypt.secretname="${APPNAME}-tls" --dry-run
-helm upgrade --install $APPNAME $APPURI --set nameOverride="${APPNAME}",frontendCertificateSource="${CERTSOURCE}",csisecrets.vaultname="${KVNAMELOWER}",csisecrets.tenantId="${KVTENANT}",csisecrets.clientId="${CSISECRET_CLIENTID}",dnsname="${DNSNAME}",appgw.frontendCertificateName="${APPNAME}-fe",appgw.rootCertificateName="${APPNAME}",letsEncrypt.issuer="${LEISSUER},letsEncrypt.secretname="${APPNAME}-tls""
+helm upgrade --install $APPNAME $APPURI --set nameOverride="${APPNAME}",frontendCertificateSource="${CERTSOURCE}",csisecrets.vaultname="${KVNAMELOWER}",csisecrets.tenantId="${KVTENANT}",csisecrets.clientId="${CSISECRET_CLIENTID}",dnsname="${DNSNAME}",appgw.frontendCertificateName="${APPNAME}-fe",appgw.rootCertificateName="${APPNAME}",letsEncrypt.issuer="${LEISSUER}",letsEncrypt.secretname="${APPNAME}-tls"
