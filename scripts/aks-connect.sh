c1155dccda7f3a7f0aa332fec323fa8b090dd1c0
Gordonby <gordon.byers@microsoft.com>
2022-01-17 09:43:02 +0000

    mistake in the kubelogin url

--- scripts/aks-connect.sh
+++ scripts/aks-connect.sh
@@ -1,7 +1,8 @@
+echo "Getting credentials for $AKSNAME in $RG"
 az aks get-credentials -n $AKSNAME -g $RG --overwrite-existing
 
 echo "Kubelogin $kubeloginversion"
-wget https://github.com/Azure/kubelogin/releases/download/${{ env.kubeloginversion }}/kubelogin-linux-amd64.zip
+wget https://github.com/Azure/kubelogin/releases/download/$kubeloginversion/kubelogin-linux-amd64.zip
 unzip kubelogin-linux-amd64.zip
 sudo mv bin/linux_amd64/kubelogin /usr/bin
 kubelogin convert-kubeconfig -l azurecli
